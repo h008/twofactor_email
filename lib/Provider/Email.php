26d7e19849779e2fb582083b117234eb5d22832e
h_008 <h_008@mac.com>
2021-09-24 12:13:53 +0900

    OTPの有効期限を設定可能に

--- lib/Provider/Email.php
+++ lib/Provider/Email.php
@@ -21,6 +21,7 @@ use OCP\IURLGenerator;
 use OCP\IUser;
 use OCP\Security\ISecureRandom;
 use OCP\Template;
+use OCP\IConfig;
 
 class Email implements IProvider, IProvidesIcons, IProvidesPersonalSettings {
 	public const STATE_DISABLED = 0;
@@ -48,13 +49,18 @@ class Email implements IProvider, IProvidesIcons, IProvidesPersonalSettings {
 	/** @var IURLGenerator */
 	private $urlGenerator;
 
+	/** @var IConfig */
+	private $config;
+
 	public function __construct(EmailService $emailService,
 								StateStorage $stateStorage,
 								ISession $session,
 								ISecureRandom $secureRandom,
 								IL10N $l10n,
 								IInitialStateService $initialStateService,
-								IURLGenerator $urlGenerator) {
+								IURLGenerator $urlGenerator,
+								IConfig $config
+								) {
 		$this->emailService = $emailService;
 		$this->stateStorage = $stateStorage;
 		$this->session = $session;
@@ -62,10 +68,52 @@ class Email implements IProvider, IProvidesIcons, IProvidesPersonalSettings {
 		$this->l10n = $l10n;
 		$this->initialStateService = $initialStateService;
 		$this->urlGenerator = $urlGenerator;
+		$this->config = $config;
 	}
 
 	private function getSessionKey(): string {
 		return 'twofactor_email_secret';
+	}
+	private function getC3gSessionKey(): string {
+		return 'twofactor_email_expired';
+	}
+	private function checkExpiredAndReturn():int {
+
+		if($this->session->exists($this->getC3gSessionKey())){
+			$expired = $this->session->get($this->getC3gSessionKey());
+			if(time() < $expired){
+				return $expired;
+			}
+		}		
+		$expiredTime = $this->config->getSystemValue('twofactor_email_expiredtime',0);
+		if($expiredTime == 0||!$expiredTime){
+			return 0;
+		}
+		$this->session->remove($this->getSessionKey());
+		$expired = time() + $expiredTime;
+		$this->session->set($this->getC3gSessionkey(),$expired);
+		return $expired;
+		
+	}
+	private function getSecretFromSession(): string {
+		if ($this->session->exists($this->getSessionKey())) {
+			return $this->session->get($this->getSessionKey());
+		}
+		return ''; 
+		
+	}
+	private function generateSecret(): string {
+		$secret = $this->secureRandom->generate(6, ISecureRandom::CHAR_DIGITS);
+		$this->session->set($this->getSessionKey(), $secret);
+		return $secret;
+	}
+	private function sendSecret(IUser $user,string $secret){
+		try {
+			$this->emailService->send($user, $secret);
+			return true;
+		} catch (\Exception $ex) {
+			return false;
+		}
 	}
 
 	/**
@@ -96,21 +144,31 @@ class Email implements IProvider, IProvidesIcons, IProvidesPersonalSettings {
 
 		$secret = $this->secureRandom->generate(6, ISecureRandom::CHAR_DIGITS);
 		$this->session->set($this->getSessionKey(), $secret);
-
 		return $secret;
 	}
+
 
 	/**
 	 * Get the template for rending the 2FA provider view
 	 */
 	public function getTemplate(IUser $user): Template {
-		$secret = $this->getSecret();
+		$expired = $this->checkExpiredAndReturn();
+		#$secret = $this->getSecret();
+		$secret = $this->getSecretFromSession();
+		if(!$secret){
+			$secret = $this->generateSecret();
+			if(!$this->sendSecret($user,$secret)){
+				return new Template('twofactor_email','error');
+			 }
 
-		try {
-			$this->emailService->send($user, $secret);
-		} catch (\Exception $ex) {
-			return new Template('twofactor_email', 'error');
 		}
+		
+
+		#try {
+		#	$this->emailService->send($user, $secret);
+		#} catch (\Exception $ex) {
+		#	return new Template('twofactor_email', 'error');
+		#}
 
 		$tmpl = new Template('twofactor_email', 'challenge');
 		$tmpl->assign('emailAddress', EmailMask::maskEmail($user->getEMailAddress()));
@@ -121,11 +179,13 @@ class Email implements IProvider, IProvidesIcons, IProvidesPersonalSettings {
 	 * Verify the given challenge
 	 */
 	public function verifyChallenge(IUser $user, string $challenge): bool {
+		$expired=$this->checkExpiredAndReturn();
 		$valid = $this->session->exists($this->getSessionKey())
 			&& $this->session->get($this->getSessionKey()) === $challenge;
 
 		if ($valid) {
 			$this->session->remove($this->getSessionKey());
+			$this->session->remove($this->getC3gSessionKey());
 		}
 
 		return $valid;
